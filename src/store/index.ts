fce631785c171cff4472737e0cbe7e5d45650db0
Izaias <izaiaslima356@gmail.com>
2023-06-28 23:51:10 -0300

    chore: set up tailwindcss

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,9 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const todoSlice = createSlice({
-	name: "todo",
-	initialState: ["POO task", "Work on Nebula", "Study Zustand"],
-	reducers: {
-		add: (state, action) => {
-			state.push(action.payload);
-		},
-	},
-});
-
 export const store = configureStore({
 	reducer: {
-		todo: todoSlice.reducer,
+
 	},
 });
-
-export const { add } = todoSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
