4113d77498a0221164bd08e92eef3356edd91177
Izaias <izaiaslima356@gmail.com>
2023-07-01 09:12:06 -0300

    chore: complete play lesson action

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,19 +1,29 @@
-import { Video } from "lucide-react";
+import { PlayCircle, Video } from "lucide-react";
 
 interface LessonProps {
 	title: string;
 	duration: string;
+	isCurrent?: boolean;
 	onPlay(): { payload: any; type: "player/play" };
 }
 
-export function Lesson({ duration, title, onPlay }: LessonProps) {
+export function Lesson({
+	duration,
+	title,
+	onPlay,
+	isCurrent = false,
+}: LessonProps) {
 	return (
 		<button
 			onClick={onPlay}
-			className="flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-300
-		transition-colors"
+			data-active={isCurrent}
+			disabled={isCurrent}
+			className={`flex items-center gap-3 text-sm enabled:hover:text-zinc-300
+		transition-colors text-zinc-400 data-[active=true]:text-emerald-400`}
 		>
-			<Video className="w-4 h-4 text-zinc-500" />
+			{!isCurrent && <Video className="w-4 h-4 text-zinc-500" />}
+			{isCurrent && <PlayCircle className="w-4 h-4 text-emerald-400" />}
+
 			<span>{title}</span>
 			<span className="ml-auto font-mono text-sx text-zinc-500">
 				{duration}
