ed325ae947e7d696ff7c3762cf307beb2a5b6e29
Izaias <izaiaslima356@gmail.com>
2023-07-01 09:40:13 -0300

    chore: complete next action

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,11 @@
 import ReactPlayer from "react-player";
 import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { next } from "../store/slicers/player";
 
 export function Video() {
+	const dispatch = useDispatch();
+
 	const lesson = useAppSelector((state) => {
 		const { currentLessonIndex, currentModuleIndex } = state.player;
 
@@ -13,11 +17,17 @@ export function Video() {
 		return currentLesson;
 	});
 
+	function handlePlayNext() {
+		dispatch(next());
+	}
+
 	return (
 		<div className="bg-zinc-950 w-full aspect-video">
 			<ReactPlayer
 				width="100%"
 				height="100%"
+				onEnded={handlePlayNext}
+				// playing
 				controls
 				url={`https://www.youtube.com/watch?v=${lesson.id}`}
 			/>
