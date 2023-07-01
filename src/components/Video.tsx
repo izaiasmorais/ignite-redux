8e67d5f7a11d36429415778ac16a394d5db8eb90
Izaias <izaiaslima356@gmail.com>
2023-07-01 08:49:46 -0300

    chore: complete update state flux

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,13 +1,25 @@
 import ReactPlayer from "react-player";
+import { useAppSelector } from "../store";
 
 export function Video() {
+	const lesson = useAppSelector((state) => {
+		const { currentLessonIndex, currentModuleIndex } = state.player;
+
+		const currentLesson =
+			state.player.course.modules[currentModuleIndex].lessons[
+				currentLessonIndex
+			];
+
+		return currentLesson;
+	});
+
 	return (
 		<div className="bg-zinc-950 w-full aspect-video">
 			<ReactPlayer
 				width="100%"
 				height="100%"
 				controls
-				url="https://youtu.be/oPOKpSFqy-I"
+				url={`https://www.youtube.com/watch?v=${lesson.id}`}
 			/>
 		</div>
 	);
